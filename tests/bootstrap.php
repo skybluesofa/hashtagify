bbda2d1b1935fadc408e3b1484075642f71134c3
Dave Rogers <connect@skybluesofa.com>
2016-09-29 18:18:22 -0500

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,3 @@
-<?php
-set_error_handler(function () {
-  echo file_get_contents(dirname(__DIR__).'/INSTALL');
-  exit(1);
-}, E_ALL);
-require_once dirname(__DIR__) . '/vendor/autoload.php';
-restore_error_handler();
+include_once('AutoLoader.php');
+// Register the directory to your include files
+AutoLoader::registerDirectory('../src');
