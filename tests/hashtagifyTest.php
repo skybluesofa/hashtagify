3a053bd13d6c5d5ab661e4eccb0ab0da22397cfc
Dave Rogers <connect@skybluesofa.com>
2016-09-29 18:34:03 -0500

    Update hashtagifyTest.php

--- tests/hashtagifyTest.php
+++ tests/hashtagifyTest.php
@@ -11,7 +11,6 @@ class HashtagifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals ('#AnotherDayAnotherDollar', Hashtagify::PascalCase ('Another day. Another dollar.'));
 		$this->assertEquals ('#AnotherDayAnotherDollar', Hashtagify::PascalCase ('ANOTHER day. Another dollar.'));
 		$this->assertEquals ('#CuriosityKilledTheCat', Hashtagify::PascalCase ('Curiosity Killed the Cat'));
-		$this->assertEquals ('#CuriosityKilledTheCat', Hashtagify::pascalCase ('Curiosity Killed the Cat'));
 	}
 	function test_camel_case_hashtags () {
 		$this->assertEquals ('#anotherDayAnotherDollar', Hashtagify::camelCase ('Another day. Another dollar.'));
@@ -22,19 +21,16 @@ class HashtagifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals ('#another_day_another_dollar', Hashtagify::lowercase_snake ('Another day. Another dollar.'));
 		$this->assertEquals ('#another_day_another_dollar', Hashtagify::lowercase_snake ('ANOTHER day. Another dollar.'));
 		$this->assertEquals ('#curiosity_killed_the_cat', Hashtagify::lowercase_snake ('Curiosity Killed the Cat'));
-		$this->assertEquals ('#curiosity_killed_the_cat', Hashtagify::lowercaseSnake ('Curiosity Killed the Cat'));
 	}
 	function test_camelcase_snake_hashtags () {
 		$this->assertEquals ('#another_Day_Another_Dollar', Hashtagify::camelcase_Snake ('Another day. Another dollar.'));
 		$this->assertEquals ('#another_Day_Another_dollar', Hashtagify::camelcase_Snake ('ANOTHER day. Another dollar.'));
 		$this->assertEquals ('#curiosity_Killed_The_Cat', Hashtagify::camelcase_Snake ('Curiosity Killed the Cat'));
-		$this->assertEquals ('#curiosity_Killed_The_Cat', Hashtagify::camelcaseSnake ('Curiosity Killed the Cat'));
 	}
 	function test_uppercase_snake_hashtags () {
 		$this->assertEquals ('#Another_Day_Another_Dollar', Hashtagify::Uppercase_Snake ('Another day. Another dollar.'));
 		$this->assertEquals ('#Another_Day_Another_dollar', Hashtagify::Uppercase_Snake ('ANOTHER day. Another dollar.'));
 		$this->assertEquals ('#Curiosity_Killed_The_Cat', Hashtagify::Uppercase_Snake ('Curiosity Killed the Cat'));
-		$this->assertEquals ('#Curiosity_Killed_The_Cat', Hashtagify::uppercaseSnake ('Curiosity Killed the Cat'));
 	}
 	function test_lowercase_hashtags () {
 		$this->assertEquals ('#anotherdayanotherdollar', Hashtagify::lowercase ('Another day. Another dollar.'));
@@ -45,7 +41,6 @@ class HashtagifyTest extends PHPUnit_Framework_TestCase {
 		$this->assertEquals ('#ANOTHERDAYANOTHERDOLLAR', Hashtagify::UPPERCASE ('Another day. Another dollar.'));
 		$this->assertEquals ('#ANOTHERDAYANOTHERDOLLAR', Hashtagify::UPPERCASE ('ANOTHER day. Another dollar.'));
 		$this->assertEquals ('#CURIOSITYKILLEDTHECAT', Hashtagify::UPPERCASE ('Curiosity Killed the Cat'));
-		$this->assertEquals ('#CURIOSITYKILLEDTHECAT', Hashtagify::uppercase ('Curiosity Killed the Cat'));
 	}
 }
 ?>
